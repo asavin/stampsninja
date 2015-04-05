110c812f642c20e29ff8af0bf7eaeaae5ebffe2f
Alex Savin <karismafilms@gmail.com>
2015-04-05 15:14:48 +0100

    Pickers

--- index.ios.js
+++ index.ios.js
@@ -10,22 +10,96 @@ var {
   StyleSheet,
   Text,
   View,
+  PickerIOS
 } = React;
 
+var PickerItemIOS = PickerIOS.Item;
+
+var COUNTRIES = {
+  uk: {
+    name: 'United Kingdom'
+  },
+  finland: {
+    name: 'Finland'
+  },
+  russia: {
+    name: 'Russia'
+  }
+}
+
 var StampsNinja = React.createClass({
+  getInitialState: function() {
+    return {
+      countryIndex: 0,
+      country: 'uk',
+      countryTo: 'finland'
+    };
+  },
+
+  updateCountry: function(country) {
+    this.setState({
+      country: country
+    });
+  },
+
+  updateCountryTo: function(country) {
+    this.setState({
+      countryTo: country
+    });
+  },
+
   render: function() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Stamps Ninja
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          From
         </Text>
+        <PickerIOS
+          style={styles.countrypicker}
+          selectedValue={this.state.country}
+          onValueChange={this.updateCountry}>
+
+          {Object.keys(COUNTRIES).map((country) => (
+            <PickerItemIOS
+              key={country}
+              value={country}
+              label={country}
+            />
+            )
+          )}
+
+        </PickerIOS>
         <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+Control+Z for dev menu
+          Currently selected: {this.state.country}
         </Text>
+
+
+        <Text style={styles.instructions}>
+          To
+        </Text>
+        <PickerIOS
+          style={styles.countrypicker}
+          selectedValue={this.state.countryTo}
+          onValueChange={this.updateCountryTo}>
+
+          {Object.keys(COUNTRIES).map((country) => (
+            <PickerItemIOS
+              key={country}
+              value={country}
+              label={country}
+            />
+            )
+          )}
+
+        </PickerIOS>
+        <Text style={styles.instructions}>
+          Currently selected: {this.state.countryTo}
+        </Text>
+
+
       </View>
     );
   }
@@ -34,9 +108,18 @@ var StampsNinja = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
+    flexWrap: 'nowrap',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#eee',
+  },
+  countrypicker: {
+    margin: 10,
+    width: 380,
+    height: 200,
+    borderWidth: 1,
+    borderColor: 'blue'
   },
   welcome: {
     fontSize: 20,
